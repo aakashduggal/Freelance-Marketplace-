2219a3a0b0a8b30ebc298cc6f336a6097865ee72
Aakash Duggal <aakashduggal2407@gmail.com>
2026-04-13 17:49:44 +0530

    added online users track engine

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,8 @@ import gigRoute from './routes/gig.route.js'
 import orderRoute from "./routes/order.route.js"
 import conversationRoute from "./routes/conversation.route.js"
 import messageRoute from "./routes/message.route.js"
+import http from 'http'
+import { Server } from 'socket.io'
 
 dotenv.config()
 
@@ -17,7 +19,34 @@ app.use(cookieParser())
 
 DBconnect()
 
+const server = http.createServer(app)
+const io = new Server(server,{
+    cors:{
+        origin: "http://localhost:3000"
+    }
+})
+
+// . Online Users Track Karne ka Engine
+let users = []
+
+const addUser = (userID, socketID)=>{
+    !users.some((user)=> user.userID === userID) && users.push({userID, socketID})
+}
+
+const removeUser = (socketID)=>{
+   users = users.filter((user)=> user.socketID !== socketID)
+}
+
+const getUser = (userID)=>{
+    return users.find((user)=> user.userID === userID)
+}
+
 const PORT = process.env.PORT || 8000
+
+app.use((req, res, next)=>{
+    req.io = io
+    next()
+})
 
 app.use("/api/auth", authRoute)
 app.use("/api/gigs", gigRoute)
@@ -25,7 +54,6 @@ app.use("/api/orders", orderRoute)
 app.use("/api/conversation", conversationRoute)
 app.use("/api/message", messageRoute)
 
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`App is Listening on the PORT http://localhost:${PORT}`)
 })
