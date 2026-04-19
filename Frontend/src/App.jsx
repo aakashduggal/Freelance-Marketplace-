a3c6b255c0286ceb249e61ca1ab4efaa80c68841
Aakash Duggal <aakashduggal2407@gmail.com>
2026-04-19 19:56:55 +0530

    started frontend, implementing redux and creating store

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-green-500 font-bold text-center mt-20">
+         FAANG Level Full-Stack Baby! 🚀
+      </h1>
+    </>
+  )
+}
+
+export default App
